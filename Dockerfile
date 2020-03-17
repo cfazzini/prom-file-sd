3c9616bd39b5d812d37ad05a9dac3158367bdefa
pisohaka <dkamovsky@gmail.com>
2020-03-17 13:50:04 +1000

    Add base auth and config from .env file

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:alpine
-LABEL maintainer="Narate Ketram <rate@dome.cloud>"
+LABEL maintainer="Dmitry Kamovsky <kd@arenadata.io>"
 WORKDIR /app
 ADD ./requirements.txt .
 RUN pip install -r requirements.txt
 ADD ./app.py .
-EXPOSE 5000
-CMD ["gunicorn", "-w", "2", "-b", "0.0.0.0:5000", "app:app"]
+EXPOSE 19216
+CMD ["gunicorn", "-w", "2", "-b", "0.0.0.0:19216", "app:app"]
--- app.py
+++ app.py
@@ -1,46 +1,71 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
+from flask_httpauth import HTTPBasicAuth
 from jsonschema import validate
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import json, os
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 MONGO_HOST = os.environ.get('MONGO_HOST', 'db')
 MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+DEFAULT_USER = os.environ.get('DEFAULT_USER', 'prometheus')
+DEFAULT_PASSWORD = os.environ.get('DEFAULT_PASSWORD', 'prometheus')
 
 app = Flask(__name__)
 api = Api(app)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == DEFAULT_USER:
+        return DEFAULT_PASSWORD
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    # return 403 instead of 401 to prevent browsers from displaying the default
+    # auth dialog
+    return make_response(jsonify({'message': 'Unauthorized access'}), 403)
+
 
 schema = {
      "type" : "object",
-     "properties" : {
-         "target" : {"type" : "string"},
-         "labels" : {"type" : "object"}
+     "properties": {
+         "target": {"type": "string"},
+         "labels": {"type": "object"}
      },
      "required": ["target"]
 }
 
 delete_schema = {
-     "type" : "object",
+     "type": "object",
      "properties" : {
-         "id" : {"type" : "string"},
+         "id": {"type": "string"},
      },
      "required": ["id"]
 }
 
-class IndexPage(Resource):
-    def get(self):
-        return { "message": "Need Web UI, Please add UI support https://github.com/narate/prom-file-sd"}
+# class IndexPage(Resource):
+#     def get(self):
+#         return {"message": "Need Web UI, Please add UI support https://github.com/narate/prom-file-sd"}
+
 
 class PromTargets(Resource):
+    decorators = [auth.login_required]
+
     def get(self):
         client = MongoClient(MONGO_HOST, MONGO_PORT)
         db = client.prom
         col = db.targets
         targets = []
         for o in col.find():
-            targets.append({'target': o['target'], 'labels': o.get('labels',{}), 'id': str(o['_id'])})
-        return { 'targets': targets }
+            targets.append({'target': o['target'], 'labels': o.get('labels', {}), 'id': str(o['_id'])})
+        return {'targets': targets}
     
     def post(self):
         body = request.get_json()
@@ -76,7 +101,7 @@ class PromTargets(Resource):
                 targets.append(
                     {
                         'targets': [o['target']],
-                        'labels': o.get('labels',{})
+                        'labels': o.get('labels', {})
                     }
                 )
     
@@ -119,7 +144,9 @@ class PromTargets(Resource):
             os.fsync(f.fileno())
         return None, 204
 
-api.add_resource(IndexPage, '/')
+# api.add_resource(IndexPage, '/')
+
+
 api.add_resource(PromTargets, '/targets')
 
 if __name__ == '__main__':
